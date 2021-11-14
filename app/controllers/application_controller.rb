e225db788f1aec9b64cc48ac14351211a0b4f129
Aristela Marku <aristela.marku@gmail.com>
2021-11-14 14:14:55 +0100

    commit tree 3b79e72510602cbf79096f18afd0bfd46408f8ce

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,28 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods'do
+   new_baked_good=BakedGood.create(
+   name: params[:name],
+   price: params[:price],
+   bakery_id: params[:bakery_id]
+   )
+   new_baked_good.to_json
+   end
+  
+
+   patch '/bakeries/:id' do
+   update_bakery=Bakery.find(params[:id])
+   update_bakery.update(
+     name: params[:name]
+   )
+   update_bakery.to_json
+   end
+
+   delete '/baked_goods/:id' do
+   item_to_delete=BakedGood.find(params[:id])
+   item_to_delete.destroy
+   item_to_delete.to_json
+   end
+
 end
